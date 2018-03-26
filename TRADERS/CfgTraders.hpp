99f5559d89027f15ac124183a4103bc45befa929
lordkabal26 <lordkabal26@hotmail.com>
2018-03-26 01:58:49 -0400

    Add files via upload

---
+++ TRADERS/CfgTraders.hpp
@@ -0,0 +1,299 @@
+	/**
+	 * Weapons, scopes, silencers, ammo
+	 */
+	class Exile_Trader_Armory
+	{
+		name = "ARMORY";
+		showWeaponFilter = 1;
+		categories[] =
+		{
+			"APEXAmmunition",
+			"APEXAssaultRifles",
+			"APEXBipodAttachments",
+			"APEXLightMachineGuns",
+			"APEXMuzzleAttachments",
+			"APEXOpticAttachments",
+			"APEXPistols",
+			"APEXSniperRifles",
+			"APEXSubMachineGuns",
+			"Ammunition",
+			"AssaultRifles",
+			"BipodAttachments",
+			"CUPAmmunition",
+			"CUPAssaultRifles",
+			"CUPLightMachineGuns",
+			"CUPMuzzleAttachments",
+			"CUPOpticAttachments",
+			"CUPPistols",
+			"CUPPointerAttachments",
+			"CUPSniperRifles",
+			"CUPSubMachineGuns",
+			"LightMachineGuns",
+			"MuzzleAttachments",
+			"OpticAttachments",
+			"Pistols",
+			"PointerAttachments",
+			"Shotguns",
+			"SniperRifles",
+			"SubMachineGuns"
+		};
+	};
+
+	/**
+	 * Satchels, nades, UAVs, static MGs
+	 */
+	class Exile_Trader_SpecialOperations
+	{
+		name = "SPECIAL OPERATIONS";
+		showWeaponFilter = 1; // for noob tubes
+		categories[] =
+		{
+			"A3LauncherAmmo",
+			"A3Launchers",
+			"APEXLauncherAmmo",
+			"ApexLaunchers",
+			"CUPExplosive",
+			"CUPLauncherAmmo",
+			"CUPLaunchers",
+			"ExplosiveWeapons",
+			"Explosives",
+			"Flares",
+			"Navigation",
+			"Smokes",
+			"StaticMGs",
+			"UAVs"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	/**
+	 * Uniforms, vests, helmets, backpacks
+	 */
+	class Exile_Trader_Equipment
+	{
+		name = "EQUIPMENT";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Backpacks",
+			"APEXBackpacks",
+			"APEXHeadgear",
+			"APEXUniforms",
+			"APEXVests",
+			"Backpacks",
+			"CUPBackpacks",
+			"CUPHeadgear",
+			"CUPUniforms",
+			"CUPVests",
+			"FirstAid",
+			"Headgear",
+			"Tools",
+			"Uniforms",
+			"Vests",
+			"APEXTools"		
+		};
+	};
+
+	/**
+	 * Cans, cans, cans
+	 */
+	class Exile_Trader_Food
+	{
+		name = "FAST FOOD";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Drinks",
+			"Food",
+			"NonVeganFood"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	/**
+	 * Light bulbs, metal, etc.
+	 */
+	class Exile_Trader_Hardware
+	{
+		name = "HARDWARE";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Hardware",
+			"ExtendedBaseMod",
+			"CBuilding"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	/**
+	 * Sells cars and general vehicles
+	 */
+	class Exile_Trader_Vehicle
+	{
+		name = "VEHICLE";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Armed",
+			"A3Cars",
+			"A3Trucks",
+			"ApexCars",
+			"Bikes",
+			"CUPArmed",
+			"CUPUnarmed",
+			"Cars",
+			"Trucks",
+			"A3Tanks"
+		};
+	};
+
+	/**
+	 * Sells choppers and planes
+	 */
+	class Exile_Trader_Aircraft
+	{
+		name = "AIRCRAFT";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Planes",
+			"A3armedChoppers",
+			"A3unarmedChoppers",
+			"ApexPlanes",
+			"ApexVTOL",
+			"CUPChoppers",
+			"Choppers",
+			"Planes",
+			"JetPlanes",
+			"Pods"
+		};
+	};
+
+	/**
+	 * Sells ships and boats
+	 */
+	class Exile_Trader_Boat
+	{
+		name = "BOAT";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"A3Boats",
+			"ApexBoats",
+			"Boats"
+			// Whatever your last line is make sure it doesn't have a comma! <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<< <<<<<<<<<<<<<<<<
+		};
+	};
+
+	class Exile_Trader_Diving
+	{
+		name = "DIVERS";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Diving"
+		};
+	};
+
+	/**
+	 * Sells Community Items
+	 */
+	class Exile_Trader_CommunityCustoms
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community"
+		};
+	};
+	class Exile_Trader_CommunityCustoms2
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community2"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms3
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community3"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms4
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community4"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms5
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community5"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms6
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community6"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms7
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community7"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms8
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community8"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms9
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community9"
+		};
+	};
+
+	class Exile_Trader_CommunityCustoms10
+	{
+		name = "COMMUNITY";
+		showWeaponFilter = 0;
+		categories[] =
+		{
+			"Community10"
+		};
+	};
